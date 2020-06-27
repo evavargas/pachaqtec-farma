12a1e9b6d5655ac073102a890cf7a9af8060db57
unknown <kafupaucarpura5@gmail.com>
2020-06-27 13:28:14 -0500

    back

--- farma/urls.py
+++ farma/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from authentication.views import Login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('farma/',include(('pachaqtecfarma.urls', 'farma')))
+    path('login/', Login.as_view(), name='login'),
+    path('farma/',include(('pachaqtecfarma.urls', 'farma'))),
 ]
--- pachaqtecfarma/views.py
+++ pachaqtecfarma/views.py
@@ -1,26 +1,10 @@
+
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.shortcuts import redirect
 from .models import Client, Product, Invoice, Resume
-<<<<<<< HEAD
-from django.views.generic import (
-    ListView, TemplateView
-)
 
 
 # Create your views here.
-
 def home(request):
-<<<<<<< HEAD
-    return render(request, "pachaqtecfarma/index.html", {})
-
-class ListProducts(ListView):
-    model = Product
-    template_name = ''
-    queryset = Product.objects.all()
-    context_object_name= 'products'
-
-
-=======
     return render(request, "forms/layout.html", {})
->>>>>>> 9c2677d12784f07d3fd868ede72b77b6c13cdeb2
