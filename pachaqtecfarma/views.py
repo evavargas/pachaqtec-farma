a26aa9cd8dc08f683587785f0145dc1a731689e6
evavargas <eva.estrella96@gmail.com>
2020-06-27 12:10:07 -0500

    models-

--- pachaqtecfarma/views.py
+++ pachaqtecfarma/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.shortcuts import redirect
 from .models import Client, Product, Invoice, Resume
-from .forms import ComentarioForm
+
 
 
 # Create your views here.
