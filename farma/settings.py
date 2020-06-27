a7183367198bdf6e1636f42a87c498e7e08ad6c8
Fernando Lira <fernando@datatrust.pe>
2020-06-27 12:26:18 -0500

    update

--- farma/settings.py
+++ farma/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'farma.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,4 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (BASE_DIR, '/static')
--- pachaqtecfarma/views.py
+++ pachaqtecfarma/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.shortcuts import redirect
 from .models import Client, Product, Invoice, Resume
-from .forms import ComentarioForm
 
 
 # Create your views here.
 def home(request):
-    return render(request, "pachaqtecfarma/index.html", {})
+    return render(request, "forms/layout.html", {})
