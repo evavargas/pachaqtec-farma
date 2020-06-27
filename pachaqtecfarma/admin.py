37a105cad751345039365d6423716ae69e0af1e1
unknown <kafupaucarpura5@gmail.com>
2020-06-27 11:37:55 -0500

    elmer

--- pachaqtecfarma/admin.py
+++ pachaqtecfarma/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Client, Product, Invoice, Resume
 
 # Register your models here.
+
+admin.site.register(Client)
+admin.site.register(Product)
+admin.site.register(Invoice)
+admin.site.register(Resume)
