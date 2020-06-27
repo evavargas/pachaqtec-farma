60581a3972ce4455ff8439808994afbbb8822184
evavargas <eva.estrella96@gmail.com>
2020-06-27 13:17:33 -0500

    tamplate SignIn

--- pachaqtecfarma/views.py
+++ pachaqtecfarma/views.py
@@ -2,12 +2,8 @@ from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.shortcuts import redirect
 from .models import Client, Product, Invoice, Resume
-<<<<<<< HEAD
-=======
-
->>>>>>> a26aa9cd8dc08f683587785f0145dc1a731689e6
 
 
 # Create your views here.
 def home(request):
-    return render(request, "forms/layout.html", {})
+    return render(request, "forms/signIn.html", {})
