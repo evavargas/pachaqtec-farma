0a1b653c97802b0067917edb6eb0840004e31577
unknown <kafupaucarpura5@gmail.com>
2020-06-27 12:28:33 -0500

    back

--- pachaqtecfarma/views.py
+++ pachaqtecfarma/views.py
@@ -2,9 +2,20 @@ from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.shortcuts import redirect
 from .models import Client, Product, Invoice, Resume
-from .forms import ComentarioForm
+from django.views.generic import (
+    ListView, TemplateView
+)
 
 
 # Create your views here.
+
 def home(request):
     return render(request, "pachaqtecfarma/index.html", {})
+
+class ListProducts(ListView):
+    model = Product
+    template_name = ''
+    queryset = Product.objects.all()
+    context_object_name= 'products'
+
+
